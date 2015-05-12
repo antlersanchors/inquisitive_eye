1e400c536282dbbfda41811c232f670a3e65d752
Michael Liston <michaelowen.liston@gmail.com>
2015-05-12 21:22:10 +0200

    pseudo-code FTW

--- inquisitive_eye.ino
+++ inquisitive_eye.ino
@@ -1,24 +1,54 @@
 //inquisitive_eye.ino
+//i love lamp
 #include <Servo.h>
 
 #define servoTopPin 10
 #define servoBottomPin 11
 
-#define potTop A0
+#define servoTopHigh 180 //define the range of the top servo's movement
+#define servoTopLow 0 
+
+#define servoBottomHigh 96 //define the values to push the bottom servo around
+#define servoBottomLow 86
+
+#define potTop A0 //just for manual control
 #define potBottom A1
 
-
-int led = 13;
+#define led 13 //because you never know when you might want to debug
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(led, OUTPUT);
+  
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(100);               // wait for a second
-    digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
-    delay(100);               // wait for a second
+
+	switch (state){
+		case 1: //inquisitiveness triggered!
+			fixate();
+			break;
+
+		default: //normal behaviour as usual
+			move();
+			break;
+	}
+   
+}
+
+void move(){
+	// for a random duration
+	// move along a path
+	// oscillate?
+
+}
+
+void fixate(){
+	// generate some random "coordinates"
+	// create a triangle of points off of this region of interest
+	// alternate position between these
+	// resume default behaviour after timer expires
+
+
 }
